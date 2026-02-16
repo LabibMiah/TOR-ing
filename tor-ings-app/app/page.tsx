51813f6c68539714495555b129fa4963b5918cb7
Labib Miah <labibmiah05@gmail.com>
2026-02-16 14:43:11 +0000

    There isnt any logins yet so i put the middleware in the app directory

--- tor-ings-app/app/page.tsx
+++ tor-ings-app/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">  Welcome To Tor-ingS</h1>
-      <p> </p>
+      <p className="text-lg">Your one-stop shop for all things Tor-ing!</p>
     </main>
   );
 }
